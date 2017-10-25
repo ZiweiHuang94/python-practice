c2077ba1b91215343e7727eab249fcdce0de3182
Ziwei Huang <z.huangcsee@gmail.com>
2017-10-25 09:40:13 +0200

    simple practice

---
+++ overiding.py
@@ -0,0 +1,10 @@
+class Parent: # define parent class
+    def myMethod(self):
+        print("Calling parent method")
+
+class Child(Parent):
+    def myMethod(self):
+        print("Calling child method")
+
+c = Child()
+c.myMethod()
